223fe94125194f1fe1085df24d8f56f0a062efdc
Dimova Vassilena <vassilena.dimova.ext@nokia.com>
2022-11-06 23:04:29 +0200

    initial commit

---
+++ my-first-spring-shop-web-app/src/main/java/org/borg/myfirstspringshopwebapp/MyFirstSpringShopWebAppApplication.java
@@ -0,0 +1,13 @@
+package org.borg.myfirstspringshopwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyFirstSpringShopWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyFirstSpringShopWebAppApplication.class, args);
+	}
+
+}
